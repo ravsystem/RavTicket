ce6817525525a37164398129c6f333aa0b6871c3
Ygo Hamon <31461178+ygohamon10@users.noreply.github.com>
2019-06-07 13:10:25 -0300

    V0.5
    listagem tickets

--- RAVTICKETS/src/br/com/window/tickts/Listar_ticket.java
+++ RAVTICKETS/src/br/com/window/tickts/Listar_ticket.java
@@ -3,8 +3,17 @@ package br.com.window.tickts;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Listar_ticket extends JInternalFrame {
+	private JTable table;
+	private JTextField textField;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -20,8 +29,51 @@ public class Listar_ticket extends JInternalFrame {
 	}
 
 	public Listar_ticket() {
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 588, 384);
+		getContentPane().setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 156, 552, 187);
+		getContentPane().add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"ID", "NV", "CLIENTE", "ASSUNTO", "SITUA\u00C7\u00C3O", "AGENTE", "ESTATUS"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(36);
+		table.getColumnModel().getColumn(1).setPreferredWidth(45);
+		table.getColumnModel().getColumn(2).setPreferredWidth(120);
+		table.getColumnModel().getColumn(3).setPreferredWidth(106);
+		table.getColumnModel().getColumn(4).setPreferredWidth(68);
+		table.getColumnModel().getColumn(5).setPreferredWidth(103);
+		table.getColumnModel().getColumn(6).setPreferredWidth(83);
+		scrollPane.setViewportView(table);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 125, 179, 23);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("BUSCAR");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(199, 124, 89, 23);
+		getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("EDITAR");
+		btnNewButton_1.setBounds(473, 125, 89, 23);
+		getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("CANCELAR");
+		btnNewButton_2.setBounds(374, 125, 89, 23);
+		getContentPane().add(btnNewButton_2);
 
 	}
-
 }
